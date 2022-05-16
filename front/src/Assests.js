0ffe50569099afa8e38420a8a2bcdd91ee81ec11
Zachary Sirna <zsirna@yahoo.com>
2022-05-16 18:15:23 -0400

    building assest component temporarily using a db.json

--- front/src/Assests.js
+++ front/src/Assests.js
@@ -1,73 +1,79 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "react-bootstrap";
 
+const Assests = () => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-      [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-      },
-      [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-      },
-    }));
-    
-    const StyledTableRow = styled(TableRow)(({ theme }) => ({
-      '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-      },
-      // hide last border
-      '&:last-child td, &:last-child th': {
-        border: 0,
-      },
-    }));
-    
-    function createData(name, calories, fat) {
-      return { name, calories, fat };
-    }
-    
-    const rows = [
-      createData('Car', 12_12_08, 22000),
-      createData('House', 1_18_10, 1000000),
-      createData('Savings Account', 8_12_06, 100000),
-      createData('401k', 9_12_07, 60000),
-      createData('IRA', 4_12_07, 50000),
-    ];
-    
-    export default function Assests() {
-      return (
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 700 }} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>Assest</StyledTableCell>
-                <StyledTableCell align="right">Date Purchased</StyledTableCell>
-                <StyledTableCell align="right"></StyledTableCell>
-                <StyledTableCell align="right"></StyledTableCell>
-                <StyledTableCell align="right">Estimated Value</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <StyledTableRow key={row.name}>
-                  <StyledTableCell component="th" scope="row">
-                    {row.name}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">{row.calories}</StyledTableCell>
-                  <StyledTableCell align="right">{row.fat}</StyledTableCell>
-                  <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-                  <StyledTableCell align="right">{row.protein}</StyledTableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      );
-    }
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  const [assests, setAssests] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3001/assests")
+      .then((resp) => resp.json())
+      .then((data) => setAssests(data));
+  }, []);
+
+  if (!assests) {
+    return <h2>LOADING......</h2>;
+  }
+
+  return (
+    <div>
+      <button>Add Assest</button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="left">Assest</StyledTableCell>
+              <StyledTableCell align="center">Date Purchased</StyledTableCell>
+              <StyledTableCell align="right">Estimated Value</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody sx={{ minWidth: 500 }}>
+            {assests.map((row) => (
+              <StyledTableRow key={row.id}>
+                {/* <StyledTableCell component="th" scope="row">
+                  </StyledTableCell> */}
+                <StyledTableCell align="left">{row.assest}</StyledTableCell>
+                <StyledTableCell align="center">
+                  {row.datePurchased}
+                </StyledTableCell>
+                <StyledTableCell align="right">
+                  {row.estimatedValue}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default Assests;
