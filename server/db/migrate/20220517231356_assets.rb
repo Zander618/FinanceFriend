cc2786fe880ee7747a735471201c3a252abc5ab4
Zachary Sirna <zsirna@yahoo.com>
2022-05-17 19:22:35 -0400

    working on backend, migrating and setting up tables

---
+++ server/db/migrate/20220517231356_assets.rb
@@ -0,0 +1,9 @@
+class Assets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :assets do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :datePurchased
+      t.integer :estimatedValue
+  end
+end
---
+++ server/db/migrate/20220517231434_expenses.rb
@@ -0,0 +1,4 @@
+class Expenses < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
