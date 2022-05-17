f262721a062e187ad48c069cb9e7f54bfa703866
Zachary Sirna <zsirna@yahoo.com>
2022-05-17 11:34:40 -0400

    removing graphs component, building and replacing with expenses component

---
+++ front/src/AddExpense.js
@@ -0,0 +1,70 @@
+import {useState} from "react";
+import "./PopUp.css";
+
+const AddExpense = ({ trigger, setTrigger, expenses, setExpenses }) => {
+  
+  const [formData, setFormData] = useState({
+    name: "",
+    monthlyCost: ""
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    fetch("http://localhost:3001/expenses", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: formData.name,
+        monthlyCost: formData.monthlyCost,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => addExpense(data));
+    setFormData({
+      name: "",
+      monthlyCost: "",
+    });
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const addExpense = (expense) => {
+    const updateMyExpenses = [...expenses, expense];
+    setExpenses(updateMyExpenses);
+  };
+  
+  return trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <h3 style={{ color: "black" }}>Add Expense</h3>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Expense:
+            <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+          </label>
+          <br></br>
+          <label>
+            Monthly Cost:
+            <input type="text" name="monthlyCost" value={formData.monthlyCost} onChange={handleChange}/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <button className="close-btn" onClick={() => setTrigger(false)}>
+          close
+        </button>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
+};
+
+export default AddExpense;
--- front/src/Expenses.js
+++ front/src/Expenses.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import AddExpense from "./AddExpense";
 
 const Expenses = () => {
-  return (
-    <div>Expenses</div>
-  )
-}
 
-export default Expenses
+  const [expenses, setExpenses] = useState();
+  const [buttonPopup, setButtonPopup] = useState(false);
+
+  // function handleDeleteClick() {
+  //   fetch(`http://localhost:3001/assets/${assets.id}`, {
+  //     method: "DELETE"
+  //   })
+  //     console.log(assets.id)
+  // }
+
+  
+  useEffect(() => {
+    fetch("http://localhost:3001/expenses")
+      .then((resp) => resp.json())
+      .then((data) => setExpenses(data));
+  }, []);
+
+  if (!expenses) {
+    return <h2>LOADING......</h2>;
+  }
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setButtonPopup(true);
+        }}
+      >
+        Add Expense
+      </button>
+      <AddExpense
+        expenses={expenses}
+        setExpenses={setExpenses}
+        trigger={buttonPopup}
+        setTrigger={setButtonPopup}
+      />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="left">Expense</StyledTableCell>
+              <StyledTableCell align="right">Monthly Cost</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody sx={{ minWidth: 500 }}>
+            {expenses.map((expense) => (
+              <StyledTableRow key={expense.id}>
+                <StyledTableCell align="left">{expense.name}</StyledTableCell>
+                <StyledTableCell align="right">
+                  ${expense.monthlyCost}
+                </StyledTableCell>
+                <StyledTableCell><button>x</button></StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default Expenses;
